e86b81af94a6946a75d4cb02410a98c431df9e4e
Walks2K <walks2000@gmail.com>
2022-03-30 12:58:06 +0100

    Implemented random score generation by simulating rounds until one team has eliminated the other.

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
 """
 Python & OOP version of https://github.com/Walks2K/CSGO-Score-Generator
 
-CSGO Score Generator - simulate CS:GO matches and generates a score
+CSGO Score Generator - simulate CS:GO matches
 
-Classes:
-    Player:
-        Player object that contains all the information about a player (name, team, kills, deaths, etc.)
-    Team:
-        Team object that contains all the information about a team (name, score, etc)
-    Game:
-        Game object that contains all the information about a game (teams, score, etc)
-
-    Functions:
-        parse_team_page:
-            Parse a team page from HLTV.org and return a Team object
-        parse_match_page:
-            Parse a match page from HLTV.org and return a Game object
+CS matches are won either by first to 16 rounds (unless 15-15, in which case the first team to get 4 ahead wins)
 """
 
 
+import copy
+import random
 import requests
 from bs4 import BeautifulSoup
 
 
 class Player:
     """
-    Player object that contains all the information about a player (name, team, kills, deaths, etc.)
+    Player object that contains all the information about a player
     """
 
     def __init__(self, name, team, kills, deaths, assists):
@@ -41,25 +31,81 @@ class Player:
 
 class Team:
     """
-    Team object that contains all the information about a team (name, score, etc)
+    Team object that contains all the information about a team
     """
 
-    def __init__(self, name, score, players):
+    def __init__(self, name, rounds_won, players):
         self.name = name
-        self.score = score
+        self.rounds_won = rounds_won
         self.players = players
 
     def __str__(self):
-        return f"{self.name}: {self.score}, {[player.name for player in self.players]}"
+        return f"{self.name}: {self.rounds_won}, {[player.name for player in self.players]}"
 
 
 class Game:
     """
-    Game object that contains all the information about a game (teams, score, etc)
+    Game object that contains all the information and logic
     """
 
     def __init__(self, teams):
         self.teams = teams
+
+    def simulate(self):
+        """
+        Simulates a CS:GO match
+        """
+        # Initialize the game
+        self.teams[0].rounds_won = 0
+        self.teams[1].rounds_won = 0
+        self.teams[0].players[0].kills = 0
+        self.teams[0].players[0].deaths = 0
+        self.teams[0].players[0].assists = 0
+        self.teams[1].players[0].kills = 0
+        self.teams[1].players[0].deaths = 0
+        self.teams[1].players[0].assists = 0
+        # Simulate the game
+        while self.teams[0].rounds_won < 16 and self.teams[1].rounds_won < 16:
+            self.simulate_round()
+        # Determine the winner
+        if self.teams[0].rounds_won > self.teams[1].rounds_won:
+            winner = self.teams[0]
+            loser = self.teams[1]
+        else:
+            winner = self.teams[1]
+            loser = self.teams[0]
+        # Print the results
+        print(f"{winner.name} won the game!")
+        print(
+            f"{winner.name} won {winner.rounds_won} rounds, {loser.name} won {loser.rounds_won} rounds"
+        )
+        print(f"{winner.name}'s team: {[player.name for player in winner.players]}")
+        print(f"{loser.name}'s team: {[player.name for player in loser.players]}")
+
+    def simulate_round(self):
+        """
+        Simulates a single round of CS:GO
+        """
+        team1 = copy.deepcopy(self.teams[0])
+        team2 = copy.deepcopy(self.teams[1])
+
+        while len(team1.players) > 1 and len(team2.players) > 1:
+            team1_player = random.choice(team1.players)
+            team2_player = random.choice(team2.players)
+
+            if random.random() < 0.5:  # team1 kills team2 player
+                team1_player.kills += 1
+                team2_player.deaths += 1
+                team2.players.remove(team2_player)
+            else:  # team2 kills team1 player
+                team2_player.kills += 1
+                team1_player.deaths += 1
+                team1.players.remove(team1_player)
+
+        if len(team1.players) > len(team2.players):
+            self.teams[0].rounds_won += 1
+        elif len(team2.players) > len(team1.players):
+            self.teams[1].rounds_won += 1
 
     def __str__(self):
         return f"Team 1: {self.teams[0]}, Team 2: {self.teams[1]}"
@@ -83,8 +129,6 @@ def parse_team_page(url):
 def parse_match_page(url):
     """
     Parse a match page from HLTV.org and return a Game object
-
-    Create Team objects from div class="lineup standard-box", players are in class="player"
     """
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -111,7 +155,7 @@ def main():
         "https://www.hltv.org/matches/2354538/g2-vs-nip-esl-pro-league-season-15"
     )
 
-    print(game)
+    game.simulate()
 
 
 if __name__ == "__main__":
