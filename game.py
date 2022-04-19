f8b26d1e284e512385bd102a5d7d2333ad659d31
Walks2K <walks2000@gmail.com>
2022-04-19 12:00:53 +0100

    Added support for overtime results Added "print_result" to handle printing outcome Added assists handling

--- game.py
+++ game.py
@@ -13,28 +13,57 @@ class Game:
 
     def __init__(self, teams):
         self.teams = teams
+        self.winner = None
+        self.loser = None
 
     def simulate(self):
         """
         Simulates a CS:GO match
         """
         # Simulate the game
-        while self.teams[0].rounds_won < 16 and self.teams[1].rounds_won < 16:
+        while self.winner is None:
             self.simulate_round()
-        # Determine the winner
-        if self.teams[0].rounds_won > self.teams[1].rounds_won:
-            winner = self.teams[0]
-            loser = self.teams[1]
-        else:
-            winner = self.teams[1]
-            loser = self.teams[0]
-        # Print the results
-        print(f"{winner.name} won the game!")
+            self.check_winner()
+
+        # Print the result
+        self.print_result()
+
+    def check_winner(self):
+        """
+        Checks if there is a winner
+        """
+        # Check for winner in regular rounds
+        if self.teams[0].rounds_won == 16 and self.teams[1].rounds_won < 15:
+            self.winner = self.teams[0]
+            self.loser = self.teams[1]
+        elif self.teams[1].rounds_won == 16 and self.teams[0].rounds_won < 15:
+            self.winner = self.teams[1]
+            self.loser = self.teams[0]
+        # Check for winner in overtime
+        elif self.teams[0].rounds_won >= 15 and self.teams[1].rounds_won >= 15:
+            if (
+                self.teams[0].rounds_won % 3 == 1
+                and self.teams[0].rounds_won > self.teams[1].rounds_won + 1
+            ):
+                self.winner = self.teams[0]
+                self.loser = self.teams[1]
+            elif (
+                self.teams[1].rounds_won % 3 == 1
+                and self.teams[1].rounds_won > self.teams[0].rounds_won + 1
+            ):
+                self.winner = self.teams[1]
+                self.loser = self.teams[0]
+
+    def print_result(self):
+        """
+        Prints the result of the game
+        """
+        print(f"{self.winner.name} won the game!")
         print(
-            f"{winner.name} won {winner.rounds_won} rounds, {loser.name} won {loser.rounds_won} rounds"
+            f"{self.winner.name} won {self.winner.rounds_won} rounds, {self.loser.name} won {self.loser.rounds_won} rounds"
         )
-        winner.print_team("kills")
-        loser.print_team("kills")
+        self.winner.print_team("kills")
+        self.loser.print_team("kills")
 
     def simulate_round(self):
         """
@@ -62,12 +91,23 @@ class Game:
             # Pick a random player from each team
             team1_player = random.choice(self.teams[0].alive_players())
             team2_player = random.choice(self.teams[1].alive_players())
+            assist = None
 
             # Roll a random chance for who will kill who
             if random.randint(0, 1) == 0:
-                self.event_kill(team1_player, team2_player, None)
+                # choose a unique random player from team1 to assist
+                while (assist is team1_player or assist is None) and len(
+                    self.teams[0].alive_players()
+                ) > 1:
+                    assist = random.choice(self.teams[0].alive_players())
+                self.event_kill(team1_player, team2_player, assist)
             else:
-                self.event_kill(team2_player, team1_player, None)
+                # choose a unique random player from team2 to assist
+                while (assist is team2_player or assist is None) and len(
+                    self.teams[1].alive_players()
+                ) > 1:
+                    assist = random.choice(self.teams[1].alive_players())
+                self.event_kill(team2_player, team1_player, assist)
 
         # End the round
         self.round_end()
